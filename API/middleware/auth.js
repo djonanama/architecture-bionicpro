83b9de09710bfa69436ffad18c17bcb8261b11b2
Evgenii Pavlov <evgenii.p@payme.uz>
2025-04-19 23:14:53 +0300

    fixed issuer

--- API/middleware/auth.js
+++ API/middleware/auth.js
@@ -7,7 +7,12 @@ const client = jwksClient({
 
 function getKey(header, callback) {
     client.getSigningKey(header.kid, function (err, key) {
-        const signingKey = key?.getPublicKey();
+        if (err || !key) {
+            console.error("Error getting signing key:", err);
+            return callback(err);
+        }
+
+        const signingKey = key.getPublicKey();
         callback(null, signingKey);
     });
 }
@@ -15,15 +20,19 @@ function getKey(header, callback) {
 function verifyToken(req, res, next) {
     const authHeader = req.headers.authorization || '';
     const token = authHeader.split(' ')[1];
+    const algorithm = process.env.API_KEYCLOAK_ALGORITHM || 'RS256';
 
     if (!token) return res.status(401).json({ message: 'Missing token' });
 
-    const algorithm = process.env.API_KEYCLOAK_ALGORITHM || 'RS256';
+    const jwtDecode = jwt.decode(token, { complete: true });
+    const issuer = jwtDecode?.payload?.iss;
 
     jwt.verify(token, getKey, {
-        issuer: `${process.env.API_KEYCLOAK_URL}/realms/${process.env.API_KEYCLOAK_REALM}`,
+        issuer: issuer,
         algorithms: [algorithm]
     }, (err, decoded) => {
+        console.log({ issuer });
+        console.log('Decoded header:', jwtDecode);
         console.error(err, decoded);
 
         if (err) return res.status(401).json({ message: 'Invalid token' });
